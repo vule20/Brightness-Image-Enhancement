eae5d5174f8a166ae556481f2acab46c92d76779
DucVu20 <leducvuvietnam@gmail.com>
2022-05-23 15:03:57 +0700

    changed input image location

---
+++ demo.m
@@ -0,0 +1,14 @@
+% sigma = 15 for htc
+clc; close all; clear all;
+addpath(genpath('./'));
+
+source_img = "./test_img/hive_ntu_5.png";
+
+sigma_BM3D = 10;
+[htc_img, htc_bm3d_img] = HTC_BM3D(source_img, sigma_BM3D);
+imshow(htc_img);
+figure
+imshow(htc_bm3d_img);
+
+
+
